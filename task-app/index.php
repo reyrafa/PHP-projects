45290a82906b9338ed296974f3c88f70b1b05734
reyrafa <rafaelcostemiano05@gmail.com>
2026-09-15 16:26:46 +0800

    add task counts

--- task-app/index.php
+++ task-app/index.php
@@ -6,19 +6,33 @@ $tasks = [
     ],
     [
         "title" => "Learn Laravel",
-        "completed" => false,
+        "completed" => true,
     ],
     [
         "title" => "Build API",
         "completed" => false,
     ],
+    [
+        "title" => "Learn Docker",
+        "completed" => false
+    ]
 ];
+$tasksCount = 0;
+$completed = 0;
+$pending = 0;
 foreach ($tasks as $task) {
     echo $task['title'] . " - ";
+    $tasksCount++;
     if ($task['completed'] === true) {
+        $completed++;
         echo "Completed";
     } else {
+        $pending++;
         echo "Pending";
     }
     echo "\n";
 }
+echo "\n";
+echo "Total tasks: $tasksCount \n";
+echo "Completed: $completed \n";
+echo "Pending: $pending \n";
