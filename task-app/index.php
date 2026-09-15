54ad7072d357ca9098f3dc9425d20bab08ed492d
reyrafa <rafaelcostemiano05@gmail.com>
2026-09-15 16:39:22 +0800

    add function

--- task-app/index.php
+++ task-app/index.php
@@ -17,22 +17,49 @@ $tasks = [
         "completed" => false
     ]
 ];
-$tasksCount = 0;
-$completed = 0;
-$pending = 0;
 foreach ($tasks as $task) {
     echo $task['title'] . " - ";
-    $tasksCount++;
     if ($task['completed'] === true) {
-        $completed++;
         echo "Completed";
     } else {
-        $pending++;
         echo "Pending";
     }
     echo "\n";
 }
+
+function countTasks(array $tasks): int
+{
+    $tasksCount = 0;
+    foreach ($tasks as $task) {
+        $tasksCount++;
+    }
+    return $tasksCount;
+}
+function countCompleted(array $tasks): int
+{
+    $completed = 0;
+    foreach ($tasks as $task) {
+        if($task['completed'] === true){
+            $completed++;
+        }
+    }
+    return $completed;
+}
+function countPending(array $tasks): int
+{
+    $pending = 0;
+    foreach ($tasks as $task) {
+       if($task['completed'] !== true){
+            $pending++;
+        }
+    }
+    return $pending;
+}
 echo "\n";
+
+$tasksCount = countTasks($tasks);
+$completed = countCompleted($tasks);
+$pending = countPending($tasks);
 echo "Total tasks: $tasksCount \n";
 echo "Completed: $completed \n";
 echo "Pending: $pending \n";
